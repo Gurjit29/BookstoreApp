775b94ad0b65ec5b1e6dcd6a0474c4bb9db52e3c
Cody.Beaty <cody.beaty@student.ufv.ca>
2019-04-09 11:47:22 -0700

    editted bash script to run yum if there is not apt (support for different linux OS flavors)

--- initial.sh
+++ initial.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-apt-get update
-apt-get install python3 -y
-apt-get install npm -y
+if apt-get update; then
+    apt-get install python3 -y
+    apt-get install npm -y
+else
+    yum install python3
+    yum install npm
+fi
 pip install -r requirements.txt
 npm install jquery
 npm install popper.js
